d93567d06f4442517f0930348f3e83ae4f7b647f
Maxim Plotnikau <maxim97dev@gmail.com>
2022-11-09 23:20:06 +0300

    Update Random Character

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then(response => console.log(response));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,8 +1,11 @@
 import md5 from "blueimp-md5";
 
 class MarvelService {
+    _url = 'https://gateway.marvel.com/v1/public/characters';
+    _apikey = '1a52cb7d60fa7704cf0638d5fb3ca8f4';
+
     getResources = async(url) => {
-        let response = await(url);
+        let response = await fetch(url);
 
         if (!response.ok) {
             throw new Error(`Could not fetch ${url}, status: ${response.status}`);
@@ -18,11 +21,38 @@ class MarvelService {
     getAllCharacters = () => {
         const timeStamp = +new Date();
         const hash = this.getHash(timeStamp);
+
         return this.getResources(
-            'https://gateway.marvel.com/v1/public/characters?' +
+             this._url + '?' +
+            'limit=9&offset=210' +
             '&ts=' + timeStamp +
-            '&apikey=1a52cb7d60fa7704cf0638d5fb3ca8f4' +
-            '&hash=' + hash);
+            '&apikey=' + this._apikey +
+            '&hash=' + hash
+        );
+    }
+
+    getCharacter = async (id) => {
+        const timeStamp = +new Date();
+        const hash = this.getHash(timeStamp);
+
+        const result = await this.getResources(
+            this._url + '/' + id + '?' +
+            '&ts=' + timeStamp +
+            '&apikey=' + this._apikey +
+            '&hash=' + hash
+        );
+
+        return this._transformCharacter(result);
+    }
+
+    _transformCharacter = (response) => {
+        return {
+            name: response.data.results[0].name,
+            description: response.data.results[0].description,
+            thumbnail: `${response.data.results[0].thumbnail.path}.${response.data.results[0].thumbnail.extension}`,
+            homepage: response.data.results[0].urls[0].url,
+            wiki: response.data.results[0].urls[1].url
+        }
     }
 }
 
